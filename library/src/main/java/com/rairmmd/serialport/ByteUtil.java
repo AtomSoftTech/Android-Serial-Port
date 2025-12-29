b273f7d5c39e23ad374e3554c837ea3d3b9fcbe6
Jason Lopez <AtomSoft@gmail.com>
2025-12-29 17:09:48 -0500

    Update method comments to English

--- library/src/main/java/com/rairmmd/serialport/ByteUtil.java
+++ library/src/main/java/com/rairmmd/serialport/ByteUtil.java
@@ -5,11 +5,11 @@ public class ByteUtil {
     private static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     /**
-     * 字符串转字节数组
-     *
-     * @param s 字符串
-     * @return 数组
-     */
+    * Converts a string to a byte array.
+    *
+    * @param s The string
+    * @return The byte array
+    */
     public static byte[] hexStringToByteArray(String s) {
         if (s.length() < 2) {
             s = "0" + s;
@@ -24,11 +24,11 @@ public class ByteUtil {
     }
 
     /**
-     * 字节数组转字符串
-     *
-     * @param hexBytes 数组
-     * @return 字符串
-     */
+    * Converts a byte array to a string.
+    *
+    * @param hexBytes The byte array.
+    * @return The resulting string.
+    */
     public static String hexBytesToString(byte[] hexBytes) {
         char[] hexChars = new char[hexBytes.length * 2];
         for (int j = 0; j < hexBytes.length; j++) {
@@ -40,21 +40,21 @@ public class ByteUtil {
     }
 
     /**
-     * 16进制字符串转int
-     *
-     * @param hexString 字符串
-     * @return int
-     */
+    * Converts a hexadecimal string to an integer.
+    *
+    * @param hexString The hexadecimal string.
+    * @return The integer value.
+    */
     public static int hexStringToInt(String hexString) {
         return Integer.parseInt(hexString, 16);
     }
 
     /**
-     * 16进制字符串转byte数组
-     *
-     * @param hexString the hex string
-     * @return byte[]
-     */
+    * Converts a hexadecimal string to a byte array.
+    *
+    * @param hexString the hexadecimal string
+    * @return byte[]
+    */
     public static byte[] hexStringToBytes(String hexString) {
         if (hexString == null || hexString.equals("")) {
             return null;
@@ -71,11 +71,11 @@ public class ByteUtil {
     }
 
     /**
-     * char转换位byte
-     *
-     * @param c char
-     * @return byte
-     */
+    * Converts a char to a byte.
+    *
+    * @param c the char to convert
+    * @return the resulting byte
+    */
     private static byte charToByte(char c) {
         return (byte) "0123456789ABCDEF".indexOf(c);
     }
